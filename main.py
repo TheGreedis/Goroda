6462eaec95eadb867839a45d3e4724bc115e5626
TheGreedis <jasp3r233@gmail.com>
2024-10-26 18:48:51 +0500

    Добавлена проверка существования региона. Если региона нет, то информация о нем не выводится.

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ def get_coordinates(city, key):
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
             country = results[0]['components']['country']
-            region = results[0]['components']['state']
-            return f"Широта: {lat}, Долгота: {lon} Страна: {country} Регион: {region}"
+
+            if 'state' in results[0]['components']:
+                region = results[0]['components']['state']
+                return f"Широта: {lat}, Долгота: {lon}\n Страна: {country}.\n Регион: {region}"
+            else:
+                return f"Широта: {lat}, Долгота: {lon}\n Страна: {country}."
         else:
             return "Город не найден"
     except Exception as e:
@@ -28,7 +32,7 @@ key = 'eb19b70c52c14d06987345d9476cca7b'
 
 window = Tk()
 window.title("Координаты городов")
-window.geometry("320x100")
+window.geometry("320x120")
 
 entry = Entry()
 entry.pack()
